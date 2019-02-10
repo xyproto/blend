cebe67f5b5e75740678c68ffd93c17a2cf46c725
Alexander F. Rødseth <rodseth@gmail.com>
2019-02-10 12:57:15 +0100

    Refactoring

--- mix.go
+++ mix.go
@@ -2,8 +2,12 @@ package mix
 
 import (
 	"github.com/xyproto/imagelib"
+	"image"
 )
 
+// Mixes two images with the given ratio. 0.5 is 50% of the first image and
+// 50% of the second image. 0.1 is 10% of the first image and 90% of the
+// second image.
 func Files(inFilename1, inFilename2, outFilename string, ratio float64) error {
 	img1, err := imagelib.Read(inFilename1)
 	if err != nil {
@@ -26,24 +30,11 @@ func Files(inFilename1, inFilename2, outFilename string, ratio float64) error {
 	return nil
 }
 
-func FilesWithOverlay(inFilename1, inFilename2, outFilename string, ratio float64) error {
-	img1, err := imagelib.Read(inFilename1)
-	if err != nil {
-		return err
-	}
-	img2, err := imagelib.Read(inFilename2)
-	if err != nil {
-		return err
-	}
-
+// Experimental mix of two images, with a given ratio and contrast.
+// Try ratio: 0.5 and contrast: 4.0.
+func DemosceneMix(img1, img2 image.Image, ratio, contrast float64) image.Image {
 	// Crossfade
-	SMul = ratio * 2.0
-	DMul = -ratio*2.0 + 2.0
-	outImage := BlendNewImage(img1, img2, OverlayMix)
-
-	err = imagelib.Write(outFilename, outImage)
-	if err != nil {
-		return err
-	}
-	return nil
+	SMul = ratio * contrast
+	DMul = 1.0 - ratio*contrast
+	return BlendNewImage(img1, img2, OverlayMix)
 }
